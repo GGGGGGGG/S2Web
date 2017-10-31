11c783f7d758f18510f197a6c15e7a51dfe04aca
cedeqien <cedekien@gmail.com>
2017-10-31 19:26:53 -0400

    changing the model to be conform with the ms

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,9 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('account_type')->nullable();
             $table->string('password');
+            $table->string('temp_password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
