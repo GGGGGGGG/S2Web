ebb8f7ed7294e4b496805a912b5c07f1901f58ca
cedeqien <cedekien@gmail.com>
2017-11-29 13:10:49 -0500

    incredible logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -28,9 +28,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                    <img src="{{ url('/img/logo.png') }}">
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
