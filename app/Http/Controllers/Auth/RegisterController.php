6f63b9cd59f880c60f5773f307a6148b995e2cbe
Cedeqien <cedekien@gmail.com>
2018-01-29 22:05:04 -0500

    Names should at least be 3 characters

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => 'required|string|max:16',
+            'username' => 'required|string|min:3|max:16',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
